4641060a53ce258cd5fe73004b2cacc5e3d22051
Branislav Bardonbranislavbardon@hotmail.de <branislavbardon@hotmail.de>
2025-04-02 23:54:20 +0200

    activeOnClic() function expand to toggle pages

--- scripts/onclick.js
+++ scripts/onclick.js
@@ -5,33 +5,44 @@ let cardBodyMainRef = document.getElementById("cardBodyMain");
 let cardBodyStatsRef = document.getElementById("cardBodyStats");
 let cardBodyEvoChainRef = document.getElementById("cardBodyEvoChain");
 
-function mainOnClick(){
+function mainOnClick() {
 
     cardBodyMainRef.style.display = "d_block";
     cardBodyStatsRef.style.display = "d_none";
     cardBodyEvoChainRef.style.display = "d_none";
 }
 
-function statsOnClick(){
+function statsOnClick() {
 
     cardBodyStatsRef.style.display = "d_block";
     cardBodyMainRef.style.display = "d_none";
     cardBodyEvoChainRef.style.display = "d_none";
 }
 
-function evochainOnClick(){
+function evochainOnClick() {
     cardBodyEvoChainRef.style.display = "d_block";
     cardBodyStatsRef.style.display = "d_none";
     cardBodyMainRef.style.display = "d_none";
 }
 
 function activeOnClick(source) {
-    console.log("Clicked on:", source);
-    // Alle aktiven Klassen entfernen
     document.querySelectorAll(".nav-link-Main-Stats-Evo").forEach(link => {
-      link.classList.remove("active");
+        link.classList.remove("active");
     });
-  
-    // Dem geklickten Element die aktive Klasse hinzufügen
     document.getElementById(source).classList.add("active");
-  }
+    pageToggle(source);
+}
+
+function pageToggle(source) {
+    let changer = ['mainOnClick', 'statsOnClick', 'evochainOnClick'];
+    let ids = ['cardBodyMain', 'cardBodyStats', 'cardBodyEvoChain'];
+
+    document.querySelectorAll(".page-Main-Stats-Evo").forEach(page => {
+        page.classList.add("d_none");
+    });
+    for (let index = 0; index < changer.length; index++) {
+        if (source == changer[index]) {
+            document.getElementById(ids[index]).classList.remove("d_none");
+        }
+    }
+}
